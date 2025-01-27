2a03585574da948f7159431d79340b675b90cbe0
DisEzMike <66011352@kmitl.ac.th>
2025-01-27 22:36:31 +0700

    remove dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home/Home';
 import { Route, Routes } from 'react-router-dom';
 import Admin from './pages/Admin/Admin';
+import View from './pages/View/View';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path="/" Component={Home} />
+				<Route path="/view" Component={View} />
 				<Route path='/admin' Component={Admin} />
 			</Routes>
 		</>
