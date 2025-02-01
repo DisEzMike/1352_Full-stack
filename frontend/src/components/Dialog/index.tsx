dc76d43323d243953cf7b7456f96649345d60253
DisEzMike <66011352@kmitl.ac.th>
2025-02-02 00:20:23 +0700

    add form

--- frontend/src/components/Dialog/index.tsx
+++ frontend/src/components/Dialog/index.tsx
@@ -7,7 +7,11 @@ import {
 	DialogTrigger,
 } from '../ui/dialog';
 
-import { faClose, faCloudUpload, faRedo } from '@fortawesome/free-solid-svg-icons';
+import {
+	faClose,
+	faCloudUpload,
+	faRedo,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import {
@@ -29,7 +33,7 @@ import {
 	SelectValueText,
 	useFileUpload,
 } from '@chakra-ui/react';
-import React, { Component, useState } from 'react';
+import React, { Component, FormEvent, useState } from 'react';
 
 import upload from '../../assets/img/upload.svg';
 
@@ -42,12 +46,38 @@ import {
 	FileUploadList,
 	FileUploadRoot,
 } from '../ui/file-upload';
+import { Controller, Form, useForm } from 'react-hook-form';
+import { object, z } from "zod"
 
+const formSchema = z.object({
+  autoDel: z.string({ message: "Auto Delete is required" }).array(),
+})
+
+type FormValues = z.infer<typeof formSchema>
 
 export default function UploadDialog(text: string, type = 0) {
-	const [files, setFiles] = useState([] as File[])
+	const [files, setFiles] = useState([] as File[]);
+	const removeFile = (index: number) =>
+		setFiles([...files.slice(0, index), ...files.slice(index + 1)]);
 
-	const removeFile = (index: number) => setFiles([...files.slice(0, index), ...files.slice(index + 1)]);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		control,
+	} = useForm<FormValues>();
+
+	const onSubmitHandle = handleSubmit((data) => {
+		const formData = new FormData();
+
+		files.map((file) => formData.append("files[]", file));
+		
+		const autoDel = !!data.autoDel ? data.autoDel[0] : "none";
+
+		formData.append("autoDel", autoDel);
+
+		
+	});
 
 	return (
 		<DialogRoot size="xl" placement="center" motionPreset="slide-in-bottom">
@@ -72,11 +102,9 @@ export default function UploadDialog(text: string, type = 0) {
 							maxW="xl"
 							alignItems="stretch"
 							maxFiles={10}
-							accept={["image/*", "video/*"]}
+							accept={['image/*', 'video/*']}
 							maxFileSize={200000000}
-
 							onFileAccept={(detail) => {
-								console.log(detail)
 								setFiles(detail.files);
 							}}
 						>
@@ -88,73 +116,108 @@ export default function UploadDialog(text: string, type = 0) {
 						</FileUploadRoot>
 					)}
 					{files.length > 0 && (
-						<Container className="box">
-							<div className="rst">
-								<Button className="resetbtn" variant="subtle" onClick={() => setFiles([])}>
-									<FontAwesomeIcon icon={faRedo} />
-									รีเซ็ต
-								</Button>
-							</div>
-							<Flex
-								className="flex-box"
-								gap={{ mdDown: 2, md: 5 }}
-								wrap="wrap"
-							>
-								<For each={files}>
-									{(file, index) => (
-										<Box
-											position="relative"
-											className="flex-box-item"
-										>
-											<img
-												className="flex-box-img"
-												src={URL.createObjectURL(file)}
-												alt="upload"
-											/>
-											<Float display={"flex"} alignItems={"center"} justifyContent={"center"} onClick={() => removeFile(index)}>
-												<Circle
-													size="5"
-													bg="red"
-													color="white"
+						<form onSubmit={onSubmitHandle}>
+							<Container className="box">
+								<div className="rst">
+									<Button
+										className="resetbtn"
+										variant="subtle"
+										onClick={() => setFiles([])}
+									>
+										<FontAwesomeIcon icon={faRedo} />
+										รีเซ็ต
+									</Button>
+								</div>
+								<Flex
+									className="flex-box"
+									gap={{ mdDown: 2, md: 5 }}
+									wrap="wrap"
+								>
+									<For each={files}>
+										{(file, index) => (
+											<Box
+												position="relative"
+												className="flex-box-item"
+												key={index}
+											>
+												<img
+													className="flex-box-img"
+													src={URL.createObjectURL(
+														file
+													)}
+													alt="upload"
+												/>
+												<Float
+													display={'flex'}
+													alignItems={'center'}
+													justifyContent={'center'}
+													onClick={() =>
+														removeFile(index)
+													}
 												>
-													<FontAwesomeIcon icon={faClose} />
-												</Circle>
-											</Float>
-										</Box>
-									)}
-								</For>
-							</Flex>
-							<br />
-							<SelectRoot
-								collection={data}
-								size="sm"
-								width="300px"
-							>
-								<SelectLabel>Auto Delete</SelectLabel>
-								<SelectTrigger>
-									<SelectValueText placeholder="เลือก" />
-								</SelectTrigger>
-								<SelectContent>
-									{data.items.map((date) => (
-										<SelectItem
-											item={date}
-											key={date.value}
+													<Circle
+														size="5"
+														bg="red"
+														color="white"
+													>
+														<FontAwesomeIcon
+															icon={faClose}
+														/>
+													</Circle>
+												</Float>
+											</Box>
+										)}
+									</For>
+								</Flex>
+								<br />
+								<Controller
+									control={control}
+									name="autoDel"
+									render={({ field }) => (
+										<SelectRoot
+											name={field.name}
+											value={field.value}
+											onValueChange={({ value }) =>
+												field.onChange(value)
+											}
+											onInteractOutside={() =>
+												field.onBlur()
+											}
+											collection={data}
+											size="sm"
+											width="300px"
 										>
-											{date.label}
-										</SelectItem>
-									))}
-								</SelectContent>
-							</SelectRoot>
-							<br />
-							<Button
-								className="btn1"
-								variant="plain"
-								w={{ mdDown: '50%' }}
-							>
-								<FontAwesomeIcon icon={faCloudUpload} />
-								อัพโหลด
-							</Button>
-						</Container>
+											<SelectLabel>
+												Auto Delete
+											</SelectLabel>
+											<SelectTrigger>
+												<SelectValueText placeholder="ไม่ลบ" />
+											</SelectTrigger>
+											<SelectContent>
+												{data.items.map((date, i) => (
+													<SelectItem
+														item={date}
+														key={date.value}
+													>
+														{date.label}
+													</SelectItem>
+												))}
+											</SelectContent>
+										</SelectRoot>
+									)}
+								/>
+								<br />
+								<Button
+									className="btn1"
+									variant="plain"
+									w={{ mdDown: '50%' }}
+									type="submit"
+								>
+									<FontAwesomeIcon icon={faCloudUpload} />
+									อัพโหลด
+								</Button>
+							</Container>
+						</form>
 					)}
 				</DialogBody>
 			</DialogContent>
@@ -164,7 +227,7 @@ export default function UploadDialog(text: string, type = 0) {
 
 const data = createListCollection({
 	items: [
-		{ label: 'ไม่ลบ', value: 'none' },
+		{ label: 'ไม่ลบ', value: 'none'},
 		{ label: '1 วัน', value: '1 day' },
 		{ label: '3 วัน', value: '3 days' },
 		{ label: '7 วัน', value: '7 days' },
