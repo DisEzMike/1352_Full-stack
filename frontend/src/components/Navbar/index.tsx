b6a4d35ab6ad23d6f2100bed351456fb91cedfb1
DisEzMike <66011352@kmitl.ac.th>
2025-02-01 15:56:40 +0700

    responsive nav

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,31 +1,46 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faCloudUpload } from "@fortawesome/free-solid-svg-icons"
-import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faHouse,
+	faCloudUpload,
+	faHamburger,
+	faBars,
+} from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
-import './nav.css'
-import { Link } from "react-router-dom";
+import './nav.css';
+import { Link } from 'react-router-dom';
 
-import UploadDialog from "../Dialog";
+import UploadDialog from '../Dialog';
+import { Box, Button, Kbd } from '@chakra-ui/react';
+
+import {
+	DrawerActionTrigger,
+	DrawerBackdrop,
+	DrawerBody,
+	DrawerCloseTrigger,
+	DrawerContent,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerRoot,
+	DrawerTitle,
+	DrawerTrigger,
+} from '../../components/ui/drawer';
 
 export default function Navbar() {
-  return (
-    <nav>
-      <Link className="logo" to="/">MEDIA KEEP</Link>
-      <div className="actions">
-        <div>
-          <FontAwesomeIcon icon={faHouse} />
-          <Link to="/">หน้าแรก</Link>
-        </div>
-        <div>
-        {/* <FontAwesomeIcon icon={faCloudUpload} />
-          <Link to="/upload">อัพโหลด</Link> */}
-          {UploadDialog("อัพโหลด")}
-        </div>
-        <div>
-          <FontAwesomeIcon icon={faHouse} />
-          <Link to="/admin">admin</Link>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<Box className="navbar" padding={{ md: '18px 60px' }}>
+			<Link className="logo" to="/">
+				MEDIA KEEP
+			</Link>
+			<Box className="actions">
+				<Box display={{mdDown: "none !important", md: "flex"}}>
+					<FontAwesomeIcon icon={faHouse} />
+					<Link to="/">หน้าแรก</Link>
+				</Box>
+				<Box>
+					{UploadDialog('อัพโหลด')}
+				</Box>
+			</Box>
+		</Box>
+	);
 }
