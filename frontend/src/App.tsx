230a8f3520979884d722cac611c941a4d7afb347
DisEzMike <66011352@kmitl.ac.th>
2025-02-08 20:30:18 +0700

    add dynamic path to view component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route path="/" Component={Home} />
-				<Route path="/view" Component={View} />
+				<Route path="/view/:id" Component={View} />
 				<Route path='/dashboard' Component={Admin} />
 			</Routes>
 		</>
